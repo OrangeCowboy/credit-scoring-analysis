cc3c33be8c7bc70d2d43020f558576d6dcbe205a
OrangeCowboy <george.kern.delaney@gmail.com>
2025-02-25 13:22:34 +0000

    refactor: Encapsulate the print-heavy ops in main() to stop execution when imported

--- scripts/import_pandas.py
+++ scripts/import_pandas.py
@@ -4,17 +4,22 @@ print(engine)
 
 credit_df = pandas.read_sql('SELECT * FROM credit_defaults', engine)
 
-print(credit_df)
-print(credit_df.info())
-print(credit_df.describe())
-print(credit_df.duplicated().sum)       # no duplicates
-print(credit_df[credit_df < 0].sum)     # no negative values where we don't want
+
+def main():
+    print(credit_df)
+    print(credit_df.info())
+    print(credit_df.describe())
+    print(credit_df.duplicated().sum)       # no duplicates
+    print(credit_df[credit_df < 0].sum)     # no negative values where we don't want
 
 
-for v in credit_df.columns:
-    print(f"\nColumn: {v}")
-    print(credit_df[v].unique())
+    for v in credit_df.columns:
+        print(f"\nColumn: {v}")
+        print(credit_df[v].unique())
 
 
-for v in credit_df.columns:
-    print(credit_df[v].value_counts())
+    for v in credit_df.columns:
+        print(credit_df[v].value_counts())
+
+if __name__ == "__main__":
+    main()
