71081a2d8297b34f42e09077f5e23dd402c54406
OrangeCowboy <george.kern.delaney@gmail.com>
2025-03-19 16:05:19 +0000

    feat: Created DF columns Total_Paid, Total_Bill & DTI_debt

--- scripts/eda.py
+++ scripts/eda.py
@@ -3,5 +3,19 @@ from import_pandas import credit_df
 
 print(credit_df.corr())
 
-correlation = credit_df['default_payment_next_month'].corr(credit_df['LIMIT_BAL'])
-print(f"The correlation between default_payment_next_month & LIMIT_BAL is:{correlation}")
+
+var1 = 'default_payment_next_month'
+var2 = 'LIMIT_BAL'
+var3 = 'EDUCATION'
+
+correlation = credit_df[var1].corr(credit_df[var2])
+print(f"The correlation between {var1} & {var2} is:{correlation}")
+
+correlation = credit_df[var1].corr(credit_df[var3])
+print(f"The correlation between {var1} & {var3} is:{correlation}")
+
+credit_df['Total_Paid'] = credit_df[['PAY_AMT1','PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6']].sum(axis=1)
+credit_df['Total_Bill'] = credit_df[['BILL_AMT1','BILL_AMT2', 'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6']].sum(axis=1)
+credit_df['DTI_debt'] = credit_df['Total_Bill'] / credit_df['Total_Paid']
+
+print((credit_df['Total_Paid'] == 0).value_counts())            # 1432 true, 5% paid $0, need to think of including 0's
